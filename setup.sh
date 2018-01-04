97fc0069fac1d92de86a13c332b04fa65cc89e87
Ubuntu <wjbotham+playground@gmail.com>
2018-01-04 02:16:45 +0000

    Add Transmission test stuff

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+add-apt-repository ppa:transmissionbt/ppa
+apt-get update
+apt-get -y install transmission-cli transmission-common transmission-daemon
 
+transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso.torrent
