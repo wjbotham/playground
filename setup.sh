c72e8c861216c180ceb12ac104b95fc209b7151c
Wesley Botham <wjbotham@gmail.com>
2018-01-03 20:41:47 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ apt-get update
 apt-get -y install transmission-cli transmission-common transmission-daemon
 
 transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso.torrent
+transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/17.10/ubuntu-17.10-server-amd64.iso.torrent
+transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/17.10/ubuntu-17.10-server-i386.iso.torrent
 
 watch -n 1 "df -h; echo; transmission-remote -n 'transmission:transmission' -l"
