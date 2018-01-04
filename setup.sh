234068b351cec90a61e44404e56eff9041f4f516
Wesley Botham <wjbotham@gmail.com>
2018-01-03 21:05:35 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ add-apt-repository ppa:transmissionbt/ppa
 apt-get update
 apt-get -y install transmission-cli transmission-common transmission-daemon
 
-transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/17.10/ubuntu-17.10-desktop-amd64.iso.torrent
-transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/16.04/ubuntu-16.04.3-desktop-amd64.iso.torrent
-transmission-remote -n 'transmission:transmission' -a http://releases.ubuntu.com/16.04/ubuntu-16.04.3-desktop-i386.iso.torrent
+transmission-remote -n 'transmission:transmission' -a http://academictorrents.com/download/5daa22057577521a378b71e0f0de6a934bd5c2ea.torrent
 
 watch -n 1 "df -h; echo; transmission-remote -n 'transmission:transmission' -l"
