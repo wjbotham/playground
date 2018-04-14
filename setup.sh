54b7eb66f56939c848892857df6801b770f9eb12
Wesley Botham <wjbotham@gmail.com>
2018-04-14 18:02:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 TORRENT=http://academictorrents.com/download/5daa22057577521a378b71e0f0de6a934bd5c2ea.torrent
-EFS_DIR=~/efs
+USER=ec2-user
+EFS_DIR=/home/$USER/efs
 DOWNLOAD_DIR=$EFS_DIR/Downloads
 EFS_FILE_SYSTEM=fs-c5f95c8d.efs.us-east-1.amazonaws.com
 
 # Download deps
-yum -y install amazon-efs-utils
-yum -y install transmission-cli transmission-common transmission-daemon
+yum -y install amazon-efs-utils transmission-cli transmission-common transmission-daemon
 
 # Mount the EFS 
 mkdir $EFS_DIR
+echo "Mounting EFS..."
 mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $EFS_FILE_SYSTEM:/ $EFS_DIR
 
 # Start torrenting
 service transmission-daemon start
+mkdir $DOWNLOAD_DIR
 transmission-remote -n 'transmission:transmission' -a $TORRENT -w $DOWNLOAD_DIR
 
 # Monitor results
