cd0d246e964b417be76eecfb684d15225a8d2645
Wesley Botham <wjbotham@gmail.com>
2018-04-14 16:57:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-add-apt-repository ppa:transmissionbt/ppa
-apt-get update
-apt-get -y install transmission-cli transmission-common transmission-daemon
+TORRENT=http://academictorrents.com/download/5daa22057577521a378b71e0f0de6a934bd5c2ea.torrent
+DOWNLOAD_DIR=.
 
-transmission-remote -n 'transmission:transmission' -a http://academictorrents.com/download/5daa22057577521a378b71e0f0de6a934bd5c2ea.torrent
+yum install transmission-cli transmission-common transmission-daemon
+service transmission-daemon start
+
+transmission-remote -n 'transmission:transmission' -a $TORRENT -w $DOWNLOAD_DIR
 
 watch -n 1 "df -h; echo; transmission-remote -n 'transmission:transmission' -l"
